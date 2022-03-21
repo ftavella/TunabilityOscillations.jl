992c86d2dae22ee164358a9c141c2c7a3357e707
ftavella <ftavella@umich.edu>
2022-03-21 08:24:17 -0500

    Work in progress for the detection of oscillations

--- src/detect_oscillations.jl
+++ src/detect_oscillations.jl
@@ -1,18 +1,13 @@
-function find_periodogram_peak(ode_solution, t_eval)
+function find_periodogram_peak(ode_solution, t_eval, f_sampling)
   nodes = size(ode_solution, 1)
-  tpoints = size(t_eval, 1)
-  peaks = zeros(nodes)
-  for i in nodes
+  peaks = fill(Float64[], nodes)
+  for i in 1:nodes
     # Remove mean from solution to avoid peak at low frequencies
-    # TODO
-    #=
     signal = ode_solution(t_eval)[i,:] .- mean(ode_solution(t_eval)[i,:])
-    pks, vals = findmaxima(signal, 5)
-    pgram = periodogram(signal; fs=tpoints)
+    pgram = periodogram(signal; fs=f_sampling)
     pks, vals = findmaxima(pgram.power, 3)
-    # Save largest peak
+    # Save largest peak in the periodogram
     peaks[i] = [pks[argmax(vals)], vals[argmax(vals)]]
-    =#
   end
   return peaks
 end
